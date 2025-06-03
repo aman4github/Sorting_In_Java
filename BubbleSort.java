cd65a5ce1bf64688bcf622a6da42a265f0b8d668
SK AMAN <skaman.dev03@gmail.com>
2025-06-03 13:00:59 +0530

    Initial Commit

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 3, 8, 8, 9, 9, 5, 1};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean flag = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    arr[j] += arr[j+1];
+                    arr[j+1] = arr[j] - arr[j+1];
+                    arr[j] -= arr[j+1];
+                    flag = true;
+                }
+            }
+            if (!flag)
+                break;
+        }
+
+        for (int i : arr) {
+            System.out.print(i + "\t");
+        }
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 3, 8, 8, 9, 9, 5, 1};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallestValueIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallestValueIndex] > arr[j]) {
+                    smallestValueIndex = j;
+                }
+            }
+            if (smallestValueIndex != i) {
+                arr[i] += arr[smallestValueIndex];
+                arr[smallestValueIndex] = arr[i] - arr[smallestValueIndex];
+                arr[i] -= arr[smallestValueIndex];
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + "\t");
+        }
+    }
+}
